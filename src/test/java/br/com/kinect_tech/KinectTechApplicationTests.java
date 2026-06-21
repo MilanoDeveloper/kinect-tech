e9afd33a5b5559d74eaafb2b3ac631774127a842
MilanoDeveloper <milano.developer@gmail.com>
2026-06-21 15:36:38 -0300

    first commit

---
+++ src/test/java/br/com/kinect_tech/KinectTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kinect_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinectTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
